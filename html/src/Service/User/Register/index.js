6c09665443f95b3ed0cb3029dc286acfff5fd29a
shilong.zhao.o <shilong.zhao.o@carmende-MacBook.local>
2019-08-24 22:08:58 +0800

    docker 整理

---
+++ html/src/Service/User/Register/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h1>注册页</h1>
+            </div>
+        );
+    }
+}
+
+export default Register
